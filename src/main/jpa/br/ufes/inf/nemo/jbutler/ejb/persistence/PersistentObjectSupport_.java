3db339cc826d6d0aeb5649b2567be05f0a44f7ad
Vítor E. Silva Souza <vitorsouza@gmail.com>
2020-05-27 18:03:49 -0300

    Update Java and WildFly versions according to developer manual.

--- src/main/jpa/br/ufes/inf/nemo/jbutler/ejb/persistence/PersistentObjectSupport_.java
+++ src/main/jpa/br/ufes/inf/nemo/jbutler/ejb/persistence/PersistentObjectSupport_.java
@@ -4,9 +4,9 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2019-01-04T17:09:22.626-0200")
+@Generated(value="Dali", date="2020-05-27T18:00:39.390-0300")
 @StaticMetamodel(PersistentObjectSupport.class)
 public class PersistentObjectSupport_ {
-	public static volatile SingularAttribute<PersistentObjectSupport, Long> id;
-	public static volatile SingularAttribute<PersistentObjectSupport, Long> version;
+	public static volatile SingularAttribute<PersistentObjectSupport, Object> id;
+	public static volatile SingularAttribute<PersistentObjectSupport, Object> version;
 }
