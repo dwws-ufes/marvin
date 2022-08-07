cab84066aac643f97cf56ceca662c17ffc51c8e7
brunogamaoli <brunogamaoli@gmail.com>
2022-08-07 00:14:48 -0300

    Ajuste visualização de enum nos index.xhtml

--- src/main/jpa/br/ufes/informatica/marvin/academicControl/domain/Request_.java
+++ src/main/jpa/br/ufes/informatica/marvin/academicControl/domain/Request_.java
@@ -8,19 +8,19 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2022-08-06T22:39:19.843-0300")
+@Generated(value="Dali", date="2022-08-07T00:03:02.519-0300")
 @StaticMetamodel(Request.class)
 public class Request_ extends PersistentObjectSupport_ {
 	public static volatile SingularAttribute<Request, Academic> requester;
+	public static volatile SingularAttribute<Request, Academic> userSituation;
 	public static volatile SingularAttribute<Request, Academic> grantor;
 	public static volatile SingularAttribute<Request, Deadline> deadline;
 	public static volatile SingularAttribute<Request, Date> requestDate;
+	public static volatile SingularAttribute<Request, Date> userSituationDate;
+	public static volatile SingularAttribute<Request, Date> responseDate;
 	public static volatile SingularAttribute<Request, EnumRequestSituation> requestSituation;
 	public static volatile SingularAttribute<Request, String> requestResponseDetailing;
 	public static volatile SingularAttribute<Request, String> observation;
 	public static volatile SingularAttribute<Request, String> localfileUniversityDegree;
 	public static volatile SingularAttribute<Request, String> localfileUseOfCredits;
-	public static volatile SingularAttribute<Request, Academic> userSituation;
-	public static volatile SingularAttribute<Request, Date> userSituationDate;
-	public static volatile SingularAttribute<Request, Date> responseDate;
 }
